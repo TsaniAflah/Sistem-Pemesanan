80dc76d225c72483f2e4bc294362d6e6cb49fbdb
TsaniAflah <tsaniaflah6@gmail.com>
2021-11-21 01:24:50 +0700

    adding some features and codes

--- src/edu/pcc/marc/demoui/ui/MainUI.java
+++ src/edu/pcc/marc/demoui/ui/MainUI.java
@@ -7,6 +7,11 @@ import javax.swing.table.TableColumnModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainUI {
     private JPanel rootPanel;
@@ -50,7 +55,6 @@ public class MainUI {
     private JRadioButton nonVegetarianRadioButton;
     private JRadioButton sehatRadioButton;
     private JRadioButton cepatSajiRadioButton;
-    private JTextField textField12;
     private JTextField minumanTextField;
     private JRadioButton asinRadioButton1;
     private JRadioButton manisRadioButton1;
@@ -66,7 +70,89 @@ public class MainUI {
     private JTextField textField13;
     private JTextField makananTextField;
     private JButton EXITButton;
+    private JButton simpanButton;
+    private JButton simpanButton1;
+    private JComboBox asalMakanan;
     private JFormattedTextField EXITFormattedTextField;
+    public ArrayList<String> pilihanUser = new ArrayList<>();
+
+    //ArrayList<String> spaghetti = new ArrayList<>(Arrays.asList("a","d","f","i"));
+    Object [][] dataMakanan;
+    Object [][] dataMinuman;
+
+    private void createTable(){
+        Object[][] data = {
+                {"Spaghetti", "10.000", "Non-vegetarian" , "Manis, asam, pedas, sehat", "Italia"},
+                {"Nasi goreng", "15.000", "Non-vegetarian", "Asin, asam,gurih, sehat", "Indonesia"},
+                {"Roti daging", "5.000", "Non-vegetarian", "Manis, asam, cepat saji", "China"},
+                {"Mie kwetiau", "10.000", "Non-vegetarian", "Pedas, gurih, sehat", "China"},
+                {"Italian salad", "15.000", "Vegetarian", "Gurih,sehat", "Italia"},
+                {"Lasagna", "15.000", "non-vegetarian", "Gurih, manis, pedas, sehat", "Italia"},
+                {"Sushi", "17.000", "Non-vegetarian", "Gurih, sehat", "Jepang"},
+                {"Kimchi", "15.000", "Non-vegetarian", "Gurih,sehat", "Korea"},
+                {"Rendang", "20.000", "Non-vegetarian", "Gurih, sehat, pedas", "Indonesia"},
+                {"Chili crab", "18.000", "Non-vegetarian", "Pedas,sehat", "Australia"},
+                {"Dimsum", "25.000", "Non-vegetarian", "Gurih,sehat", "Hongkong"},
+                {"Tortilla", "17.000", "Vegetarian", "Gurih,sehat,pedas", "Spanyol"}
+        };
+        dataMakanan=data;
+
+        Object[][] data1 = {
+                {"Ocha", "10.000", "M", "Dingin", "Non-coffein", "Pahit", "Jepang"},
+                {"Bubble tea", "13.000", "M", "Dingin", "Non-coffein", "Manis" ,"Taiwan"},
+                {"Café mexicano", "8.000", "M", "Panas", "Coffein", "Manis", "Mexico"},
+                {"Limoncello", "8.000", "L", "Netral", "Non-coffein",  "Asam", "Italia"},
+                {"Kopi sanger", "10.000", "S", "Panas", "Coffein", "Pahit", "Indonesia"},
+                {"Thai tea", "15.000", "M", "Dingin", "Coffein", "Manis", "Thailand"},
+                {"Espresso", "12.000", "S", "Panas", "Coffein", "Pahit", "Amerika"},
+                {"Coklat aztek", "10.000", "M", "Dingin", "Non-Coffein", "Pahit", "Spanyol"},
+                {"Cendol", "12.000", "L", "Dingin", "Non-coffein", "Manis", "Indonesia"},
+                {"Bajigur", "8.000", "M", "Panas", "Non-coffein", "Manis", "Indonesia"},
+                {"Lahang", "8.000", "M", "Panas", "Non-coffein", "Manis", "Indonesia"},
+                {"Bir pletok", "10.000", "L", "Panas", "Non-coffein", "Manis", "Indonesia"}
+        };
+        dataMinuman=data1;
+
+        //nama-nama kolom dari tabel tersebut
+        showTable.setModel(new DefaultTableModel(
+                data,
+                new String[]{"Makanan", "Harga(IDR)", "Jenis", "Tag", "Asal"}
+        ));
+
+        showTable2.setModel(new DefaultTableModel(
+                data1,
+                new String[]{"Minuman", "Harga(IDR)", "Ukuran", "Kondisi", "Jenis", "Tag", "Asal"}
+        ));
+
+        //menentukan besar satu kolom dalam tabel
+        TableColumnModel coloumns = showTable.getColumnModel();
+        coloumns.getColumn(3).setMinWidth(169);
+        coloumns.getColumn(2).setMinWidth(105);
+        coloumns.getColumn(1).setMinWidth(50);
+        coloumns.getColumn(0).setMinWidth(143);
+
+
+        TableColumnModel coloumns1 = showTable2.getColumnModel();
+        coloumns1.getColumn(0).setMinWidth(142);
+        coloumns1.getColumn(2).setMinWidth(2);
+        coloumns1.getColumn(4).setMinWidth(100);
+
+        //membuat align center
+        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+        centerRenderer.setHorizontalAlignment(SwingConstants.CENTER);
+        coloumns.getColumn(1).setCellRenderer(centerRenderer);
+        coloumns.getColumn(2).setCellRenderer(centerRenderer);
+        coloumns.getColumn(3).setCellRenderer(centerRenderer);
+        coloumns.getColumn(4).setCellRenderer(centerRenderer);
+
+        coloumns1.getColumn(1).setCellRenderer(centerRenderer);
+        coloumns1.getColumn(2).setCellRenderer(centerRenderer);
+        coloumns1.getColumn(3).setCellRenderer(centerRenderer);
+        coloumns1.getColumn(4).setCellRenderer(centerRenderer);
+        coloumns1.getColumn(5).setCellRenderer(centerRenderer);
+        coloumns1.getColumn(6).setCellRenderer(centerRenderer);
+
+    }
 
     //mengisi text field sesuai dengan check box yang di centang
     public MainUI(){
@@ -438,9 +524,176 @@ public class MainUI {
                 System.exit(0);
             }
         });
+
+        asamRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+
+        simpanButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                /**
+                if (asamRadioButton.isSelected()){
+                    pilihanUser.add("a");
+                }else if(asinRadioButton.isSelected()){
+                    pilihanUser.add("b");
+                }else if(gurihRadioButton.isSelected()) {
+                    pilihanUser.add("c");
+                }else if(manisRadioButton.isSelected()) {
+                    pilihanUser.add("d");
+                }else if(pahitRadioButton.isSelected()) {
+                    pilihanUser.add("e");
+                }else if(pedasRadioButton.isSelected()) {
+                    pilihanUser.add("f");
+                }else if(vegetarianRadioButton.isSelected()){
+                    pilihanUser.add("g");
+                }else if(nonVegetarianRadioButton.isSelected()){
+                    pilihanUser.add("h");
+                }else if(sehatRadioButton.isSelected()){
+                    pilihanUser.add("i");
+                }else if(cepatSajiRadioButton.isSelected()){
+                    pilihanUser.add("j");
+                }**/
+
+                if(vegetarianRadioButton.isSelected()){
+                    if(sehatRadioButton.isSelected()){
+                        if(gurihRadioButton.isSelected()){
+                            if (asalMakanan.getSelectedItem().equals("Italia")||asalMakanan.getSelectedItem().equals("--Pilih--")){
+                                pilihanUser.add("Italian salad");
+                            }if(asalMakanan.getSelectedItem().equals("Spanyol")||asalMakanan.getSelectedItem().equals("--Pilih--")){
+                                pilihanUser.add("Tortilla");
+                            }
+                        }else if(pedasRadioButton.isSelected()){
+                            if(asalMakanan.getSelectedItem().equals("Spanyol")||asalMakanan.getSelectedItem().equals("--Pilih--")){
+                                pilihanUser.add("Tortilla");
+                            }
+                        }
+                    }
+                }else if(nonVegetarianRadioButton.isSelected()){
+                    if(sehatRadioButton.isSelected()){
+                        if(asamRadioButton.isSelected()){
+                            if (asalMakanan.getSelectedItem().equals("Italia")||asalMakanan.getSelectedItem().equals("--Pilih--")){
+                                pilihanUser.add("Spaghetti");
+                            } if(asalMakanan.getSelectedItem().equals("Indonesia")||asalMakanan.getSelectedItem().equals("--Pilih--")){
+                                pilihanUser.add("Nasi goreng");
+                            }
+                        else if(asinRadioButton.isSelected()){
+                                if(asalMakanan.getSelectedItem().equals("Indonesia")||asalMakanan.getSelectedItem().equals("--Pilih--")){
+                                    pilihanUser.add("Nasi goreng");
+                                }
+                            }
+                        }else if(gurihRadioButton.isSelected()){
+                            if (asalMakanan.getSelectedItem().equals("China")||asalMakanan.getSelectedItem().equals("--Pilih--")){
+                                pilihanUser.add("Mie kwetiau");
+                            } if(asalMakanan.getSelectedItem().equals("Italia")||asalMakanan.getSelectedItem().equals("--Pilih--")){
+                                pilihanUser.add("Lasagna");
+                            } if(asalMakanan.getSelectedItem().equals("Indonesia")||asalMakanan.getSelectedItem().equals("--Pilih--")){
+                                pilihanUser.add("Rendang");
+                                pilihanUser.add("Nasi goreng");
+                            }if(asalMakanan.getSelectedItem().equals("Jepang")||asalMakanan.getSelectedItem().equals("--Pilih--")){
+                                pilihanUser.add("Sushi");
+                            }if(asalMakanan.getSelectedItem().equals("Korea")||asalMakanan.getSelectedItem().equals("--Pilih--")){
+                                pilihanUser.add("Kimchi");
+                            }if (asalMakanan.getSelectedItem().equals("Hongkong")||asalMakanan.getSelectedItem().equals("--Pilih--")){
+                                pilihanUser.add("Dimsum");
+                            }
+                        }else if(manisRadioButton.isSelected()){
+                            if (asalMakanan.getSelectedItem().equals("China")||asalMakanan.getSelectedItem().equals("--Pilih--")){
+                                pilihanUser.add("Roti daging");
+                            }if(asalMakanan.getSelectedItem().equals("Italia")||asalMakanan.getSelectedItem().equals("--Pilih--")){
+                                pilihanUser.add("Lasagna");
+                            }
+                        }else if(pedasRadioButton.isSelected()){
+                            if(asalMakanan.getSelectedItem().equals("China")||asalMakanan.getSelectedItem().equals("--Pilih--")){
+                                pilihanUser.add("Mie kwetiau");
+                            } if(asalMakanan.getSelectedItem().equals("Italia")||asalMakanan.getSelectedItem().equals("--Pilih--")){
+                                pilihanUser.add("Lasagna");
+                            }if(asalMakanan.getSelectedItem().equals("Indonesia")||asalMakanan.getSelectedItem().equals("--Pilih--")){
+                                pilihanUser.add("Rendang");
+                            }if (asalMakanan.getSelectedItem().equals("Australia")||asalMakanan.getSelectedItem().equals("--Pilih--")){
+                                pilihanUser.add("Chili crab");
+                            }
+                    }else if(cepatSajiRadioButton.isSelected()){
+                        if(asamRadioButton.isSelected()){
+                            if (asalMakanan.getSelectedItem().equals("China")||asalMakanan.getSelectedItem().equals("--Pilih--")){
+                                pilihanUser.add("Roti daging");
+                                }
+                            }
+                        }
+                    }
+                }
+                Set<String>setPilihanUser = new HashSet<>(pilihanUser);
+                pilihanUser = new ArrayList<>(setPilihanUser);
+                ArrayList<Integer>hargaMakanan = new ArrayList<>(setPilihanUser.size());
+                for (int i = 0; i < setPilihanUser.size(); i++) {
+                    for (int j = 0; j < dataMakanan.length; j++) {
+                        if (pilihanUser.get(i).equalsIgnoreCase(dataMakanan[j][1].toString())){
+                            hargaMakanan.add(i,Integer.parseInt((dataMakanan[j][1].toString())));
+                        };
+                    }
+                }
+                int budgetUser = Integer.parseInt(textField14.getText());
+                for (int i = 0; i < hargaMakanan.size(); i++) {
+                    if (hargaMakanan.get(i) > budgetUser){
+                        hargaMakanan.remove(i);
+                        pilihanUser.remove(i);
+                    }
+                }
+                String textHasil = "";
+                for (int i = 0; i < pilihanUser.size(); i++) {
+                    if(i==pilihanUser.size()-1){
+                        textHasil+=pilihanUser.get(i);
+                    break;
+                    }
+                    textHasil+=pilihanUser.get(i)+", ";
+                }
+                textField7.setText(textHasil);
+                rootPanel.repaint();
+            }
+        });
+
+
+        /**
+        //minuman
+        simpanButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (asinRadioButton1.isSelected()){
+                    pilihanUser.add("a");
+                }else if(manisRadioButton1.isSelected()){
+                    pilihanUser.add("b");
+                }else if(pahitRadioButton1.isSelected()) {
+                    pilihanUser.add("c");
+                }else if(mRadioButton.isSelected()) {
+                    pilihanUser.add("d");
+                }else if(sRadioButton.isSelected()) {
+                    pilihanUser.add("e");
+                }else if(lRadioButton.isSelected()) {
+                    pilihanUser.add("f");
+                }else if(netralRadioButton.isSelected()){
+                    pilihanUser.add("g");
+                }else if(dinginRadioButton.isSelected()){
+                    pilihanUser.add("h");
+                }else if(panasRadioButton.isSelected()){
+                    pilihanUser.add("i");
+                }else if(kofeinRadioButton.isSelected()){
+                    pilihanUser.add("j");
+                }else if(nonKofeinRadioButton.isSelected()){
+                    pilihanUser.add("k");
+                }
+            }
+        });**/
     }
 
+
     //sistem rekomendasi belum siap
+
+
+    //String[]question
 
     public JPanel getRootPanel() {
         return rootPanel;
@@ -455,76 +708,7 @@ public class MainUI {
         frame.setVisible(true);
     }
 
+
     //mengisi setiap kolom dalam tabel
-    private void createTable(){
-        Object[][] data = {
-                {"Spaghetti", "10.000", "Non-vegetarian" , "Manis, asam, pedas, sehat", "Italia"},
-                {"Nasi goreng", "15.000", "Non-vegetarian", "Asin, asam,gurih, sehat", "Indonesia"},
-                {"Roti daging", "5.000", "Non-vegetarian", "Manis, asam, cepat saji", "China"},
-                {"Mie kwetiau", "10.000", "Non-vegetarian", "Pedas, gurih", "China"},
-                {"Italian salad", "15.000", "Vegetarian", "Gurih,sehat", "Italia"},
-                {"Lasagna", "15.000", "non-vegetarian", "Gurih, manis, pedas", "Italia"},
-                {"Sushi", "17.000", "Non-vegetarian", "sehat", "Jepang"},
-                {"Kimchi", "15.000", "Non-vegetarian", "Gurih,sehat", "Korea"},
-                {"Rendang", "20.000", "Non-vegetarian", "Gurih, sehat, pedas", "Indonesia"},
-                {"Chili crab", "18.000", "Non-vegetarian", "Pedas,sehat", "Australia"},
-                {"Dimsum", "25.000", "Non-vegetarian", "Gurih,sehat", "Hongkong"},
-                {"Tortilla", "17.000", "Vegetarian", "Gurih,sehat,pedas", "Spanyol"}
-        };
 
-        Object[][] data1 = {
-                {"Ocha", "10.000", "M", "Dingin", "Non-coffein", "Pahit", "Jepang"},
-                {"Bubble tea", "13.000", "M", "Dingin", "Non-coffein", "Manis" ,"Taiwan"},
-                {"Café mexicano", "8.000", "M", "Panas", "Coffein", "Manis", "Mexico"},
-                {"Limoncello", "8.000", "L", "Netral", "Non-coffein",  "Asam", "Italia"},
-                {"Kopi sanger", "10.000", "S", "Panas", "Coffein", "Pahit", "Indonesia"},
-                {"Thai tea", "15.000", "M", "Dingin", "Coffein", "Manis", "Thailand"},
-                {"Espresso", "12.000", "S", "Panas", "Coffein", "Pahit", "Amerika"},
-                {"Coklat aztek", "10.000", "M", "Dingin", "Non-Coffein", "Pahit", "Spanyol"},
-                {"Cendol", "12.000", "L", "Dingin", "Non-coffein", "Manis", "Indonesia"},
-                {"Bajigur", "8.000", "M", "Panas", "Non-coffein", "Manis", "Indonesia"},
-                {"Lahang", "8.000", "M", "Panas", "Non-coffein", "Manis", "Indonesia"},
-                {"Bir pletok", "10.000", "L", "Panas", "Non-coffein", "Manis", "Indonesia"}
-        };
-
-        //nama-nama kolom dari tabel tersebut
-        showTable.setModel(new DefaultTableModel(
-                data,
-                new String[]{"Makanan", "Harga(IDR)", "Jenis", "Tag", "Asal"}
-        ));
-
-        showTable2.setModel(new DefaultTableModel(
-                data1,
-                new String[]{"Minuman", "Harga(IDR)", "Ukuran", "Kondisi", "Jenis", "Tag", "Asal"}
-        ));
-
-        //menentukan besar satu kolom dalam tabel
-        TableColumnModel coloumns = showTable.getColumnModel();
-        coloumns.getColumn(3).setMinWidth(169);
-        coloumns.getColumn(2).setMinWidth(105);
-        coloumns.getColumn(1).setMinWidth(50);
-        coloumns.getColumn(0).setMinWidth(143);
-
-
-        TableColumnModel coloumns1 = showTable2.getColumnModel();
-        coloumns1.getColumn(0).setMinWidth(142);
-        coloumns1.getColumn(2).setMinWidth(2);
-        coloumns1.getColumn(4).setMinWidth(100);
-
-        //membuat align center
-        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
-        centerRenderer.setHorizontalAlignment(SwingConstants.CENTER);
-        coloumns.getColumn(1).setCellRenderer(centerRenderer);
-        coloumns.getColumn(2).setCellRenderer(centerRenderer);
-        coloumns.getColumn(3).setCellRenderer(centerRenderer);
-        coloumns.getColumn(4).setCellRenderer(centerRenderer);
-
-        coloumns1.getColumn(1).setCellRenderer(centerRenderer);
-        coloumns1.getColumn(2).setCellRenderer(centerRenderer);
-        coloumns1.getColumn(3).setCellRenderer(centerRenderer);
-        coloumns1.getColumn(4).setCellRenderer(centerRenderer);
-        coloumns1.getColumn(5).setCellRenderer(centerRenderer);
-        coloumns1.getColumn(6).setCellRenderer(centerRenderer);
-
-    }
 }
